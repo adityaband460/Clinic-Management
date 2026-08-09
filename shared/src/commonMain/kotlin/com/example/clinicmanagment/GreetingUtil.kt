36f045af40f10739de198121a42f571d86d4f0c8
adityaband460 <adityaband460@gmail.com>
2026-08-09 20:44:11 +0530

    Initial commmit

---
+++ shared/src/commonMain/kotlin/com/example/clinicmanagment/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.clinicmanagment
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/example/clinicmanagment/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.clinicmanagment
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
